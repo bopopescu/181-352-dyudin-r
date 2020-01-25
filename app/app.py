0f992ee9d8d480d3bda9271e3e1525a8893613b7
Roman <gloukish@gmail.com>
2020-01-25 12:00:34 +0300

    signup[process]

--- app/app.py
+++ app/app.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect,request
+import mysql.connector
+import flask_login
+from config import *
+
+
 app = Flask(__name__)
 application = app
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
-@app.route('/signup')
+
+@app.route('/signup',methods=['GET','POST'])
 def signup():
+  cnx = mysql.connector.connect(**cg)
+  cursor = cnx.cursor(named_tuple=True)
+
+  username = request.args.get('username')
+  password = request.args.get('password')
+  name = request.args.get('name')
+  role = request.args.get('role')
+  data = (username, password,name,role)
+
+  if username and password:
+    query = '''INSERT INTO `user`(`username`, `password`,`name`,`role`) 
+               VALUES (%s,%s,%s,%s)'''
+    cursor.execute(query, data)
+    cnx.commit()
+    return redirect(url_for('index'))
+  cursor.close()
+  cnx.close()
+
   return render_template('signup.html')
+
 
 @app.route('/seepage')
 def seepage():
---
+++ app/config.py
@@ -0,0 +1,11 @@
+cg = {
+    'user': 'std_879',
+    'password': 'Logati22',
+    'host': 'std-mysql.ist.mospolytech.ru',
+    'database': 'std_879',
+    'raise_on_warnings': True
+}
+
+SECRET_KEY = b'w\xdc-z\x10\x81~\x04\x1d\x8a\x97\x8f\x85\xfe\x0cC'
+#CSRF_ENABLED активирует предотвращение поддельных межсайтовых запросов.
+CSRF_ENABLED = True
