e66fab2d1ab084f3d3ac331302972ab1b8f67db6
Roman <gloukish@gmail.com>
2020-01-25 11:17:10 +0300

    database is ready for use

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 application = app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
+
+@app.route('/seepage')
+def seepage():
+  return render_template('seepage.html')
